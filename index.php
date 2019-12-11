90f59992277f97336008083b62739728fc708187
Dibyendu Das <dibyendu.das.in@gmail.com>
2019-12-11 14:37:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -207,7 +207,7 @@
           <p style="font-family: 'Tangerine', cursive; font-size: 100px; text-align: center; margin-top: 200px">It's time to test your <span class="info">accuracy</span><span id="game-on">Continue</span></p>
           <a class="rope"></a>
         <?php } else { ?>
-          <div style="z-index: 4; top: 250px" class="fb-login-button" data-width="" data-size="medium" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true" data-scope="email" onlogin="afterFbLogin"></div>
+          <div style="z-index: 4; top: 250px" class="fb-login-button" data-width="" data-size="medium" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true" data-scope="email" data-onlogin="afterFbLogin"></div>
         <?php } ?>
 
     </div>
